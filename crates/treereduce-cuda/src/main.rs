f20758632b4b68d907178f287efa1d43c5e5f3f5
Gregory Blike <greg.blike@inkanitolabs.com>
2025-10-25 22:34:05 +0200

    Add cuda

---
+++ crates/treereduce-cuda/src/main.rs
@@ -0,0 +1,23 @@
+use std::collections::HashMap;
+
+use anyhow::Result;
+
+fn main() -> Result<()> {
+    treereduce::cli::main(
+        tree_sitter_cuda::LANGUAGE.into(),
+        tree_sitter_cuda::NODE_TYPES,
+        // ("parameter_declaration", &["int x"][..]),
+        HashMap::from([
+            ("compound_statement", &["{}"][..]),
+            ("number_literal", &["0"][..]),
+            ("parameter_list", &["()"][..]),
+            ("primitive_type", &["int"][..]),
+            ("return_statement", &["return;"][..]),
+            ("string_literal", &["\"\""][..]),
+            // Notes:
+            //
+            // - function_definition is optional wherever it appears, no need
+            //   to replace
+        ]),
+    )
+}
